45ca6a8cc7431002ee162cb98fd67094c67f437c
Jonathon McKitrick <jmckitrick@gmail.com>
2015-12-10 13:39:49 -0500

    Add non-functioning test

--- test/models/deal_test.rb
+++ test/models/deal_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class DealTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  # default_deal = {name: 'Great Deal',
+  #                 address: Address.new,
+  #                 short_description: 'desc',
+  #                 amount_to_raise: 10000,
+  #                 close_timeline: 'Dec 2014'}
+
+  # test "cannot accept deals closing before oct 2014" do
+  #   deal = Deal.new(default_deal)
+  #   assert_not deal.save
   # end
 end
